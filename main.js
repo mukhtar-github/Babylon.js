c23f86c7895da72afcc8b3bac98fc7d93dbe7da5
Mukhtar <mukhtartanimugarba@gmail.com>
2020-09-25 02:37:22 +0100

    Files updated

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var myBox = BABYLON.MeshBuilder.CreateBox("myBox", {height: 5, width: 2, depth: 0.5}, scene);
+// var myBox = BABYLON.MeshBuilder.CreateBox("myBox", {height: 5, width: 2, depth: 0.5}, scene);
 
-var myPoints = [
-    new BABYLON.Vector3(0, 0, 0),
-    new BABYLON.Vector3(0, 1, 1),
-    new BABYLON.Vector3(0, 1, 0)
-];
+// var myPoints = [
+//     new BABYLON.Vector3(0, 0, 0),
+//     new BABYLON.Vector3(0, 1, 1),
+//     new BABYLON.Vector3(0, 1, 0)
+// ];
+
+
+scene.ambientColor = new BABYLON.Color3(1, 1, 1);
+
